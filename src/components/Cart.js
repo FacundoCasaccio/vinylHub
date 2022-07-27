8a84c290b24947ccc68b5240bc10062a02cfa75a
Facundo Casaccio <casacciofacundo@gmail.com>
2022-07-26 22:06:51 -0300

    Creada funcionalidad de finalizar compra. Los productos son agregados a firebase. El formulario para captar los datos del usuario y validaciones aun no fueron implementadas. Estilos aun no aplicados correctamente

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,15 +3,43 @@ import { useContext } from "react";
 import { cartContext } from "../context/CartContext";
 import { Link } from 'react-router-dom';
 import './styles/Cart.css'
+import { db } from "../firebase/firebase";
+import { collection, addDoc, serverTimestamp } from "firebase/firestore";
+import { useState } from "react";
 
 const Cart = () => {
 
     const { products, getProductsQuantity, removeProduct, clear, getTotalPrice } = useContext(cartContext);
+    const [purchaseId, setPurchaseId] = useState("");
+
+    const customerPlaceholder = {
+        customerName: "name",
+        email: "email", 
+        phone: "phone"
+    }
+
+    const finishPurchase = () => {
+        const purchaseOrder = {
+            customer: customerPlaceholder,
+            products: [...products],
+            date: serverTimestamp(),
+            total: getTotalPrice()
+        }
+        const purchaseOrders = collection(db, "PurchaseOrders");
+        addDoc(purchaseOrders, purchaseOrder)
+        .then( (result) => {
+            setPurchaseId(result.id);
+        })
+    }
 
     return (
 
         products.length === 0 ? 
+
         <h2>Your cart is empty, click <Link to="/"><u>here</u></Link> to continue purchasing</h2>
+        :
+        purchaseId !== "" ?
+        <h2>Thank you for your purchase! Your order id is: {purchaseId}</h2>
         :
         <>
             <div className="cart">
@@ -31,8 +59,17 @@ const Cart = () => {
                         </div>
                         ))}
                 </div>
-
+                
                 <h3>Total: ${getTotalPrice()}</h3>
+
+                    <label for="name">Enter your name...</label>
+                    <input type="text" name="name"/>
+                    <label for="email">Enter your email...</label>
+                    <input type="text" name="name"/>
+                    <label for="name">Enter your phone number...</label>
+                    <input type="text" name="name"/>
+                    <button onClick={finishPurchase}>Complete purchase</button>
+                
             </div>
         </>
     )
